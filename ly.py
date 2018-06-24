c44de5b7e16f314d3d518d84bb72414176c5010a
fonxian <fonxian2015@163.com>
2018-06-25 02:35:32 +0800

    增加注册人员爬取

--- ly.py
+++ ly.py
@@ -6,46 +6,21 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-    # response = request.urlopen("http://jzsc.mohurd.gov.cn/dataservice/query/comp/list")
-    # html = response.read()
-    # html = html.decode("utf-8")
-    # soup = BeautifulSoup(html,"html.parser")
-    #
-    # # print(soup.title.string)
-    # # print(soup.p.string)
-    # # print(soup.find_all('a'))
-    # # print(soup.get_text())
-    #
-    # print("-----------")
-    # test = soup.select("body > div.main_box.nav_mtop > div.mtop > table > tbody > tr")
-    #
-    # for i in test:
-    #     print("xxxxxxxxxxxxxxxxxxxxxx")
-    #     tdlist = i.find_all("td")
-    #     for j in tdlist:
-    #         if '企业名称' in str(j):
-    #             print("企业名称:"+j.get_text().strip())
-    #         if '统一社会信用代码' in str(j):
-    #             print("统一社会信用代码:" + j.get_text().strip())
-    #         if '企业法定代表人' in str(j):
-    #             print("企业法定代表人:" + j.get_text().strip())
-    #         if '企业注册属地' in str(j):
-    #             print("企业注册属地:" + j.get_text().strip())
-    #         # print(j.get_text().strip())
-    #     print("xxxxxxxxxxxxxxxxxxxxxx")
-
 
 def a():
     for i in range(2):
-        reqa(i)
+        req_list(i)
         time.sleep(1)
 
-def reqa(current):
+
+# 请求列表页
+def req_list(current):
     content = {'$reload': '0', '$pg': current, '$pgsz': '15'}
     res = requests.post('http://jzsc.mohurd.gov.cn/dataservice/query/comp/list', data=content)
-    parsea(res.text)
+    parse_list(res.text)
 
-def parsea(html):
+# 解析列表页
+def parse_list(html):
     soup = BeautifulSoup(html, "html.parser")
     test = soup.select("body > div.main_box.nav_mtop > div.mtop > table > tbody > tr")
     for i in test:
@@ -67,16 +42,20 @@ def parsea(html):
         print("xxxxxxxxxxxxxxxxxxxxxx")
     return
 
+
+#详情页
 def b():
     url = 'http://jzsc.mohurd.gov.cn/dataservice/query/comp/compDetail/001607230057395100'
     reqb(url)
 
+# 请求详情页
 def reqb(url):
     reqa = request.urlopen(url)
     html=reqa.read()
     html = html.decode("utf-8")
     parseb(html)
 
+# 解析详情页
 def parseb(html):
     soup = BeautifulSoup(html,"html.parser")
     test = soup.select("div.plr table tbody tr")
@@ -95,6 +74,8 @@ def parseb(html):
                 print("企业经营地址："+j.get_text().strip())
             print("-------------")
 
+
+# 请求详情页 - 企业资质资格
 def c():
      url = 'http://jzsc.mohurd.gov.cn/dataservice/query/comp/caDetailList/001607230057395100?_=1529592388332'
      reqb = request.urlopen(url)
@@ -103,6 +84,7 @@ def c():
      parsec(html)
 
 
+# 解析详情页 - 企业资质资格
 def parsec(html):
     soup = BeautifulSoup(html,"html.parser")
     test = soup.select("#catabled tbody tr")
@@ -124,6 +106,76 @@ def parsec(html):
             if "发证机关" in str(j):
                 print("发证机关"+j.get_text().strip())
 
+# 请求详情页 - 注册人员
+def req_register():
+    url = 'http://jzsc.mohurd.gov.cn/dataservice/query/comp/regStaffList/001607220057394803'
+    html = request.urlopen(url).read().decode("utf-8")
+    parse_register(html)
+
+def parse_register(html):
+    soup = BeautifulSoup(html, "html.parser")
+    test = soup.select("body table tbody tr")
+    for i in test:
+        tds = i.select("td")
+        print("----------------")
+        for j in tds:
+            if "身份证号" in str(j):
+                print("身份证号:"+j.get_text().strip())
+            if "注册类别" in str(j):
+                print("注册类别:"+j.get_text().strip())
+            if "注册号" in str(j):
+                print("注册号（执业印章号）:"+j.get_text().strip())
+            if "注册专业" in str(j):
+                print("注册专业:"+j.get_text().strip())
+        print("----------------")
+
+# 请求详情页 - 工程项目
+def req_project():
+    url = 'http://jzsc.mohurd.gov.cn/dataservice/query/comp/compPerformanceListSys/001607220057358999'
+    html = request.urlopen(url).read().decode("utf-8")
+    parse_project(html)
+
+# 请求详情页 - 工程项目
+def parse_project(html):
+    soup = BeautifulSoup(html, "html.parser")
+    test = soup.select("body table tbody tr")
+    for i in test:
+        tds = i.select("td")
+        print("----------------")
+        for j in tds:
+            if "项目编码" in str(j):
+                print("项目编码:" + j.get_text().strip())
+            if "项目名称" in str(j):
+                print("项目名称:" + j.get_text().strip())
+            if "项目属地" in str(j):
+                print("项目属地:" + j.get_text().strip())
+            if "项目类别" in str(j):
+                print("项目类别:" + j.get_text().strip())
+            if "建设单位" in str(j):
+                print("建设单位:" + j.get_text().strip())
+        print("----------------")
+
+# 请求详情页 - 不良行为
+def req_good():
+    url = 'http://jzsc.mohurd.gov.cn/dataservice/query/comp/compPerformanceListSys/001607220057358999'
+    html = request.urlopen(url).read().decode("utf-8")
+    parse_project(html)
+
+# 请求详情页 - 良好行为
+def req_good():
+    url = 'http://jzsc.mohurd.gov.cn/dataservice/query/comp/compPerformanceListSys/001607220057358999'
+    html = request.urlopen(url).read().decode("utf-8")
+    parse_project(html)
+
+
+# 请求详情页 - 黑名单记录
+def parse_good():
+    pass
+
+# 请求详情页 - 变更记录
+def req_change():
+    pass
+
 
 
     #http://jzsc.mohurd.gov.cn/dataservice/query/comp/caDetailList/001607230057395100?_=1529328120435
@@ -139,6 +191,9 @@ def parsec(html):
 if __name__ == "__main__":
     # a()
     # b()
-    c()
+    # c()
+    req_register()
+    req_project()
     pass
+
 
